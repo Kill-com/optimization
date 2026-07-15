4974ad477c2090d6dcfd4d55faca2c9b042f7797
Kirt <asanvit07@gmail.com>
2026-07-15 03:16:16 +0300

    create the first classes for to start programm execut4e method an function. Do not debuging

---
+++ analisis/c_analisis.cc
@@ -0,0 +1,14 @@
+#include <chrono>
+
+#include "c_analisis.h"
+
+//получение тактов процессора
+uint64_t CycleCounter::rdtsc() {
+        #ifdef __x86_64__
+        unsigned int lo, hi;
+        __asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
+        return ((uint64_t)hi << 32) | lo;
+        #else
+        return 0;
+        #endif
+}
---
+++ exect/execution.cc
@@ -0,0 +1,7 @@
+
+
+#include "execution.h"
+
+void EXECUTER::exect(std::string_view method, std::string_view function){
+
+}
