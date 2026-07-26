8fc590ff3e57883af73871d0b129d6af7d674eae
kvshavlenko <150430919+kvshavlenko@users.noreply.github.com>
2026-07-27 05:56:29 +0700

    Update gold_sech.cpp
    шаблонная функция

--- gold_sech.cpp
+++ gold_sech.cpp
@@ -1,20 +1,14 @@
-#define _USE_MATH_DEFINES
-#include <functional>
 #include <cmath>
 #include <iostream>
-#include <numbers>          
+const double TAU = 0.6180339887498949;  // золотое сечение
 
-const float EPS = 1e-6;
-const float e = M_E; 
-const double TAU = 0.6180339887498949;   
+template<typename T, typename Func>
+T golden_section(Func f, T a, T b) {
+    T x1 = a + (1 - TAU) * (b - a);
+    T x2 = a + TAU * (b - a);
+    T fx1 = f(x1);
+    T fx2 = f(x2);
 
-
-extern "C" {
-float f(std::function<float(float)> f, float a, float b) {
-    float x1 = a + (1 - TAU) * (b - a);
-    float x2 = a + TAU * (b - a);
-    float fx1 = f(x1);
-    float fx2 =  f(x2);
     std::cout << "a=" << a << ", x1=" << x1 << ", x2=" << x2 << ", b=" << b << std::endl;
 
     while ((b - a) > EPS) {
@@ -35,5 +29,4 @@ float f(std::function<float(float)> f, float a, float b) {
     }
 
     return (a + b) / 2;
-}
 }
