6d52868f8f317c40518b5e96c9eefc56965b3a41
ch33mss <mrchms303@gmail.com>
2026-08-13 21:57:05 +0300

    добавил комменты

--- container/container.hpp
+++ container/container.hpp
@@ -5,43 +5,62 @@
 
 #include "controller_container.hpp"
 
+
+//базовый шаблонный класс для хранения любого объекта T. Основа контейнеров
 template<typename T>
 class Icontainer{
 private:
     T info;
 public:
+//конструктор с переменным числом аргументов
     template<typename ...Args>
     Icontainer(Args&&...args):info(std::forward<Args>(args)...){};
+//преобразование к типу Т
     operator T(){return info;};
+//получение НЕконстантной ссылки на хранимое значение
     T& getinfo(){return info;};
+//получение КОНСТАНТНОЙ ссылки на хранимое значение
     const T& getinfo() const { return info; }
+//метод изменяющий хранимое значение
     void inputinfo(T&& i){info=i;};
 };
+
+//класс, хранящий вектор элементов типа Т
+//наследует базовые методы контейнера и методы работы с векторами
 template<typename T>
 class IcontainerVector:public Icontainer<std::vector<T>>,
     public ControlVector<IcontainerVector,T>{
 public:
+//конструктор для готового вектора
     IcontainerVector(std::vector<T> info):
     Icontainer<std::vector<T>>(info){};
+//конструктор для создания вектора поэлементно (переменное число аргументов)
     template<typename ...Args>
     IcontainerVector(Args&&...args):Icontainer<std::vector<T>>(std::forward<Args>(args)...){};
 };
 
-
+//класс для хранения строки
 class containerStr:public Icontainer<std::string>{
 public:
     using Icontainer<std::string>::Icontainer;
 };
+
+//класс для хранения вектора строк
 class containerVectorStr:public IcontainerVector<std::string>{
 public:
     using IcontainerVector<std::string>::IcontainerVector;
 };
+
+//шаблонный класс для хранения кортежа
 template<typename ...Args>
 class ContainerValue:public Icontainer<std::tuple<Args...>>,
     public ConrolTuple{
 public:
     using Icontainer<std::tuple<Args...>>::Icontainer;
 };
+
+// Универсальный шаблонный класс-контейнер для хранения значения любого типа T
+//можно использовать для создания именованных типов контейнеров
 template<typename T>
 class containerClass: public Icontainer<T>{
 public:
--- container/controller_container.hpp
+++ container/controller_container.hpp
@@ -7,17 +7,22 @@
 template<template<typename...> class Derived, typename T>
 class ControlVector:public ControlDerived<Derived<T>>{
 public:
+//смотрим размер вектора
     size_t getsize(){
         return this->derived_ptr->getinfo().size();
     }
+//доступ к элементам вектора по индексу
     T operator[](size_t index) {
         return this->derived_ptr->getinfo()[index];
     }
 };
 
+//класс для работы с кортежами
 class ConrolTuple{
 public:
 template<typename T>
+
+//приведение к кортежу
 static auto to_tuple(T&& t) {
     if constexpr (std::is_same_v<std::decay_t<T>, std::tuple<>>) {
         return std::forward<T>(t);
@@ -26,6 +31,7 @@ static auto to_tuple(T&& t) {
     }
 }
 
+//перегрузка to_tuple для rvalue ссылок (просто пересылает кортеж дальше, не изменяяя)
 template<typename... Ts>
 static auto to_tuple(std::tuple<Ts...>&& t) {
     return std::forward<std::tuple<Ts...>>(t);
