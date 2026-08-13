5a71ae844c0c030201d072a2f59e96d3c6d5ff93
Kirt <asanvit07@gmail.com>
2026-08-13 20:55:32 +0300

    add comments start_controller

--- controller/start_controller/execution.hpp
+++ controller/start_controller/execution.hpp
@@ -15,21 +15,42 @@ public:
     }
 };
 
+/**
+ * @tparam Classes Класс в котором происходит основной вызов exect
+ * @tparam Args Типы аргументов value
+ */
 template<template<typename...> class Classes, typename ...Args>
-class StartCalculation:public ICollectStart, public IStartCalculation{
+class StartCalculation:public ICollectStart{
 private:
-    ContainerValue<Args...> value;
-    using FirstType = typename std::tuple_element<0, std::tuple<Args...>>::type;
+    ContainerValue<Args...> value; //Хранение value
+    using FirstType = typename std::tuple_element<0, std::tuple<Args...>>::type; //Получение первого типа в Args
 public:
+    //конструктор по умолчанию
     StartCalculation(){};
+    /**
+     * @brief Construct a new Start Calculation object
+     * 
+     * @param method_name Имя метода
+     * @param function_name Имена функций
+     */
     template<typename ...Func>
     StartCalculation(std::string method_name, Func&&... function_name):
         ICollectStart(method_name,std::forward<Func>(function_name)...)
     {};
+    /**
+     * @brief Сохранение любых аргументов в value для вызова
+     * Они используютсья при вызове как начальные точки
+     * 
+     * @param u аргументы
+     */
     template<typename ...U>
     void input_value(U&&...u){
         value=ContainerValue<Args...>(std::forward<U>(u)...);
     }
+    /**
+     * @brief Вызывает метод с его функциями и аргументами
+     * 
+     */
     void operator()(){
         StartPlug<Classes<Args...>> startplug(value, target_function);
         std::string method_name = method;
